bda5631865834fb2b79a5d288cc18666a1710d6e
john-smilga <johnsmilga@gmail.com>
2021-06-26 08:32:51 -0700

    06-Sidebar Before Recording

---
+++ 06-sidebar/final/app.js
@@ -0,0 +1,18 @@
+const getElement = (selector) => {
+  const el = document.querySelector(selector)
+  if (el) return el
+  throw new Error(`Please check your classes : ${selector} does not exist`)
+}
+
+const sidebarToggle = getElement('.sidebar-toggle')
+const sidebar = getElement('.sidebar')
+const closeBtn = getElement('.close-btn')
+
+sidebarToggle.addEventListener('click', function () {
+  sidebar.classList.toggle('show-sidebar')
+})
+
+closeBtn.addEventListener('click',()=>{
+   sidebar.classList.remove('show-sidebar')
+
+})
