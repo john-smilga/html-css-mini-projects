1909a60cf52e0dbee594a7a8a8e5cb5adc3f02bc
john-smilga <johnsmilga@gmail.com>
2021-07-24 08:32:41 -0700

    08-questions added

---
+++ 08-questions/final/app.js
@@ -0,0 +1,25 @@
+const getElement = (selector, list) => {
+  const el = list
+    ? [...document.querySelectorAll(selector)]
+    : document.querySelector(selector);
+
+  // check if only one element
+  if (list && el.length === 1) return el[0];
+  // check if list is not empty
+  if (list && el.length > 0) return el;
+  // if not a list and element exists
+  if (!list && el) return el;
+  throw new Error(`Please double check the ${selector}`);
+};
+
+// traversing the dom
+const btns = getElement('.question-btn', true);
+const title = getElement('.title');
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
+
+    question.classList.toggle('show-text');
+  });
+});
